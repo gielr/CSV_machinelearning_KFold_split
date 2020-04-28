a247ce8ca40fd8bd8a6aec2e7336ca8a7e3914d8
Sainti <giel.robert@gmail.com>
2020-04-28 13:45:16 +0200

    Changes in file name

--- CSV - machinelearning 6 - KFold + split.py
+++ CSV - machinelearning 6 - KFold + split.py
@@ -38,7 +38,7 @@ def adjust(opis_train, kategoria_train, opis_test, kategoria_test):
 
 
 # Start programu
-dataset = pd.read_csv('Files\Dane-do-walidacji.csv', delimiter=';', encoding='utf-8')
+dataset = pd.read_csv('Files\Dane-do-nauki.csv', delimiter=';', encoding='utf-8')
 datasetValidation = pd.read_csv('Files\Dane-do-walidacji.csv', delimiter=';', encoding='utf-8')
 
 results = xlsxwriter.Workbook("Wyniki.xlsx")
